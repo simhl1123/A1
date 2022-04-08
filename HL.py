a68d8b84072aedf51be44d38c6fab37d5097d5c7
simhl1123 <77651941+simhl1123@users.noreply.github.com>
2022-04-08 15:41:42 +0800

    Add files via upload

--- HL.py
+++ HL.py
@@ -124,9 +124,3 @@ def fetchInfo():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
-
-
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
